a04507448fff1dc7b81e3ef366a71bb7c0bcc6c1
Luan Orlandi <orlandi.luan@gmail.com>
2018-06-11 23:29:25 -0300

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <section className="hero">
+    <div className="hero-body">
+      <div className="container">
+        <div className="column is-8 is-offset-2">
+          <h1 className="title">
+            Lorem ipsum
+          </h1>
+          <h2 className="subtitle">
+            Lorem ipsum
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, non non platea eu, neque vel massa
+            corporis quis, aliquip integer. Dolor in felis. Et bibendum orci
+            justo, praesent eget feugiat at nibh pede, sodales consequat urna
+            dolor, primis eget mi, egestas elit. Eget luctus ac in proin id ut.
+            Orci vivamus, tempor nunc, quis duis dictum. Placerat vestibulum
+            consequat consectetuer pellentesque. Ipsum eu pellentesque amet
+            habitant id.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default IndexPage
